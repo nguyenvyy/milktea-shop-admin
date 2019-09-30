73dd2e11ac822f781ae3735fd1e553eed3d6dcc7
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-09-30 22:49:56 +0700

    add something package

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 const App: React.FC = () => {
+
   return (
     <div className="App">
       <header className="App-header">
