9222694c0f8f5b3e145712d1e04d2d29b768b73a
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-10-03 14:41:04 +0700

    update gitlab-ci

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        "helo"
+        "helo gan song"
       </header>
     </div>
   );
