e1102ae77990794348ef07dce4b81b6e9b75753a
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2019-11-18 14:18:52 +0700

    add profile page

---
+++ src/components/ManageProfile/AccountInfo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const AccountInfo = () => {
+
+    return (
+        <div>
+            account
+        </div>
+    )
+}
---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Profile.scss'
+import { Header } from '../../components/common/Header/Header'
+
+const Profile = () => {
+    return (
+        <div className="profile">
+            <Header title="Profile"  />
+        </div>
+    )
+}
+
+export default Profile;
